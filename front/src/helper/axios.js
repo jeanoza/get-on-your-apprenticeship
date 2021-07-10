40d13c9bec1aae743a930f2958c7df535cb63b11
jeanoza <choikb916@gmail.com>
2021-07-10 07:20:39 +0200

    fix(axios.js): change back-tik to quotes

--- front/src/helper/axios.js
+++ front/src/helper/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? `https://teolia.herokuapp.com/` : "http://localhost:3000"
-})
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://teolia.herokuapp.com/"
+      : "http://localhost:3000",
+});
